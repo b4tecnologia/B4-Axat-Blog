b2bbba56edc9a3d432887f5d6411a7645f557068
Wilgner Luis Pereira Barreto <49105241+WilgnerB@users.noreply.github.com>
2025-09-14 08:45:34 -0300

    Update Strapi config files and add Docker deployment script
    Added a resource file with Docker commands to deploy Strapi config files. Updated admin.js to provide a default ADMIN_JWT_SECRET, plugins.js to configure users-permissions with a default JWT secret, and server.js to adjust the app keys assignment.

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -2,11 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-<<<<<<< Updated upstream
-    keys: env.array('APP_KEYS'),
-=======
     keys: [env('APP_KEYS')],
->>>>>>> Stashed changes
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
