344a7cf6d8fe5da79b4454ac44633bd2be542e42
Wilgner Luis Pereira Barreto <49105241+WilgnerB@users.noreply.github.com>
2025-09-18 00:09:58 -0300

    Update vite.config.js

--- strapi/src/admin/vite.config.js
+++ strapi/src/admin/vite.config.js
@@ -3,11 +3,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   server: {
     allowedHosts: [
-      'blog.axat.com.br',
-      'localhost',
-      '127.0.0.1'
+      'blog.axat.com.br'
     ],
-    host: '0.0.0.0', // Allow external connections
+    host: 'blog.axat.com.br', // Allow external connections
     port: 1337
   }
 })
